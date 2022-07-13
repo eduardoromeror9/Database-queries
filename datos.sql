20215c85caccc22ee8099c60f3cb797a8bdd5515
Eduardo Romero <eduardoromeror9@outlook.com>
2022-07-13 07:07:45 -0400

    Insertar en tablas

--- datos.sql
+++ datos.sql
@@ -44,7 +44,7 @@ desc nombreTabla;
 show full columns from nombreTabla;
 
 
---* Crear una tabla ejemplos
+--* Crear tablas ejemplos
 create table if not exists books (
     book_id int unsigned primary key auto_increment,
     author_id int unsigned,
@@ -63,3 +63,67 @@ create table if not exists authors (
     name varchar(100) not null,
     nationality varchar(3)
 );
+
+create table clients (
+    client_id integer unsigned primary key auto_increment,
+    `name` varchar(50) not null,
+    email varchar(100) not null unique,
+    birthdate datetime,
+    gender ENUM('M', 'F', 'ND') not null,
+    active tinyint(1) not null default 1,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp
+);
+
+create table if not exists operations (
+    operation_id integer unsigned primary key auto_increment,
+    book_id integer unsigned not null,
+    client_id integer unsigned not null,
+    type ENUM('vendido', 'alquilado', 'devuelto') not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp,
+    finished tinyint(1) not null
+);
+
+--* Insertar datos en una tabla ejemplos
+insert into authors(author_id, name, nationality)
+values(null, 'Romulo Gallegos', 'VEN');
+
+insert into authors(name, nationality)
+values('Salvador Garmendia', 'VEN');
+
+inser into authors
+values(null, 'Arturo U Pietri', 'VEN');
+
+insert into authors(name, nationality)
+values('Julio Cortazar', 'ARG'), 
+('Isabel Allende', 'CHI'),
+('Octavio Paz', 'MEX'),
+('Juan Rulfo','MEX');
+
+
+
+insert into authors(name, nationality)
+values('Henning Mankel','SWE'),
+('Jaideva Goswami','USA'),
+('John Foreman','USA'),
+('Stephen Hawking','USA'),
+('Stephen Dubner','USA'),
+('Edward Said','USA'),
+('Vladimir Vapnik','RUS'),
+('V P Menon','IND'),
+('Leonard Mlodinow','USA'),
+('Frank Shih','JAP'),
+('Maria Konnikova','RUS'),
+('Sebastian Gutierrez','ESP'),
+('Kurt Vonnegut','USA'),
+('Cedric Villani','FRA'),
+('Gerald Sussman','USA'),
+('Abraham Eraly','IND'),
+('Frank Kafka','AUT'),
+('John Pratt','USA'),
+('Robert Nisbet','USA'),
+('H. G. Wells',"ENG"),
+('Werner Heisenberg','DEU'),
+('Terence Tao',"AUS"),
+('Drew Conway',"USA")
